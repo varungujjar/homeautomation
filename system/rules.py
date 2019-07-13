dc2d3896681817bb0f972cd0367663c7f030bc70
varungujjar <varun@kenosis.in>
2019-07-13 23:38:46 +0530

    And Condition Backend Mutliple conditions now working

--- system/rules.py
+++ system/rules.py
@@ -32,35 +32,40 @@ def eventsHandler(id=None):
     for ruleData in getRules:
         validateIfCondition = validateIf(ruleData)
         validateAndCondition = validateAnd(ruleData)
+        checkifActive = ruleData["trigger"]
+        ruleID = ruleData["id"]
+        ValidStatus = False
+        triggerStatus = False
         if validateIfCondition and validateAndCondition:
+            ValidStatus = True
+        if ValidStatus and checkifActive == 1:
+            setRuleTriggerStatus(ruleID,0)
+            triggerStatus = True
+        elif not ValidStatus and checkifActive == 0:
+            setRuleTriggerStatus(ruleID,1)    
+        else:
+            pass    
+        if triggerStatus:
             loop = asyncio.get_event_loop()
-            loop.create_task(doThen(ruleData))
-            
+            loop.create_task(doThen(ruleData))    
     logger.debug("Rule Check Completed")
     
  
-
 def validateIf(ruleData):
     ruleDataJson = ruleData["rule_if"]
-    checkifActive = ruleData["trigger"]
     ruleID = ruleData["id"]
-    ifValidStatus = False
-    triggerStatus = False
-    
+    ValidStatus = False
     for deviceCondition in ruleDataJson:
         conditionProperties = deviceCondition["properties"]
         conditionType = deviceCondition["condition"]
         deviceType = deviceCondition["type"]
         deviceId = deviceCondition["id"]
-
         if deviceType == "device":
             getDevice = dbGetDevice(None,None,None,deviceId)
-
             if getDevice:
                 getDeviceModule = str(getDevice["type"])
                 getDeviceClass = str(getDevice["type"])
                 getDeviceComponent = str(getDevice["component"])
-
                 try:
                     buildComponentPath = "components."+getDeviceComponent+"."+getDeviceModule
                     addSystemPath = "../components/"+getDeviceComponent
@@ -68,33 +73,65 @@ def validateIf(ruleData):
                     importModule = __import__(buildComponentPath, fromlist=getDeviceModule)
                     importDeviceClass = getattr(importModule, getDeviceClass)
                     deviceClass = importDeviceClass()
-                    ifValidStatus = deviceClass.validateProperties(getDevice,conditionProperties,conditionType) 
+                    ValidStatus = deviceClass.validateProperties(getDevice,conditionProperties,conditionType)
                 except ImportError as error:
                     logger.error("%s" % str(error)) 
                 except Exception as exception:
-                    logger.error("%s" % str(exception))
-                    
+                    logger.error("%s" % str(exception))    
             else:
                 logger.error("Device with %s Not Found" % str(deviceId))
                 pass             
-
         else:
             logger.warning("No Valid Handlers Found for Rule")
-
-        if ifValidStatus and checkifActive == 1:
-            setRuleTriggerStatus(ruleID,0)
-            triggerStatus = True
-        elif not ifValidStatus and checkifActive == 0:
-            pass 
-            setRuleTriggerStatus(ruleID,1)    
-        else:
-            pass
-        logger.debug("Rule ID %d %s" % (ruleID, str(ifValidStatus)))
-        return triggerStatus    
+    logger.debug("Rule If Condition %d %s" % (ruleID, str(ValidStatus)))
+    return ValidStatus    
 
 
 def validateAnd(ruleData):
-    return True
+    ruleDataJson = ruleData["rule_and"]
+    ruleID = ruleData["id"]
+    ValidStatus = False
+    ValidStatusArray = []
+    for deviceCondition in ruleDataJson:
+        conditionProperties = deviceCondition["properties"]
+        conditionType = deviceCondition["condition"]
+        deviceType = deviceCondition["type"]
+        deviceId = deviceCondition["id"]
+        if deviceType == "device":
+            getDevice = dbGetDevice(None,None,None,deviceId)
+            if getDevice:
+                getDeviceModule = str(getDevice["type"])
+                getDeviceClass = str(getDevice["type"])
+                getDeviceComponent = str(getDevice["component"])
+                try:
+                    buildComponentPath = "components."+getDeviceComponent+"."+getDeviceModule
+                    addSystemPath = "../components/"+getDeviceComponent
+                    sys.path.append(addSystemPath) 
+                    importModule = __import__(buildComponentPath, fromlist=getDeviceModule)
+                    importDeviceClass = getattr(importModule, getDeviceClass)
+                    deviceClass = importDeviceClass()
+                    deviceConditionStatus = deviceClass.validateProperties(getDevice,conditionProperties,conditionType)
+                    ValidStatusArray.append(deviceConditionStatus) 
+                except ImportError as error:
+                    logger.error("%s" % str(error)) 
+                except Exception as exception:
+                    logger.error("%s" % str(exception))         
+            else:
+                logger.error("Device with %s Not Found" % str(deviceId))
+                pass             
+        else:
+            logger.warning("No Valid Handlers Found for Rule")
+
+    if(len(ValidStatusArray) > 0):
+        if(len(ValidStatusArray) == 1):
+            ValidStatus = ValidStatusArray[0]
+        else:
+            ValidStatus = all(x == ValidStatusArray[0] for x in ValidStatusArray)
+    else:
+        ValidStatus = True
+
+    logger.debug("Rule And Condition %d %s" % (ruleID, str(ValidStatus)))
+    return ValidStatus
 
 
 async def doThen(ruleData):
