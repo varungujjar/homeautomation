37890e0d2b6d7ca699961e336da6550580d3ea6b
Varun Gujjar <varun@kenosis.in>
2020-02-23 14:17:55 +0530

    Wifi reconnects automatically
    Automatically connects to wifi if disconnected checks every 15 seconds

--- core/network/status.py
+++ core/network/status.py
@@ -15,27 +15,29 @@ TIMER = 15 #seconds
 logger = formatLogger(__name__)
 
 def pingGateway():
-    response = os.popen("ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && echo 'connected' || echo 'disconnected'").read()
-    return response.rstrip()
+    response_ip = subprocess.Popen("ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && echo 'connected' || echo 'disconnected'", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).stdout.read().decode('utf-8').rstrip()
+    return response_ip
 
 
 async def networkHandler():
     while True:
         getPing = pingGateway()
-        if getPing == "disconnected":
-            logger.error("Network Status => Disconnected)")
-            await networkRestart()
-        elif getPing == "connected":            
+        if getPing == "connected":            
             logger.info("Network Status => Connected")
+        else:
+            logger.warning("Network Status => Disconnected")
+            await networkRestart()
         await asyncio.sleep(TIMER)    
     
 
 async def networkRestart():
-    cmd = ["sudo","/etc/init.d/networking","restart"]
-    logger.warning("Trying to Restart Network...")
-    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    response = proc.stdout.read().decode('utf-8').rstrip()
-    logger.info("Network Restarted!")
+    logger.warning("Trying to Reconnect...")
+
+    cmd_down = ["sudo","ifconfig","wlan0", "down"]
+    subprocess.Popen(cmd_down, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.read()
+    cmd_up = ["sudo","ifconfig","wlan0", "up"]
+    proc_up = subprocess.Popen(cmd_up, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    response = proc_up.stdout.read().decode('utf-8').rstrip()
     return response
 
 
